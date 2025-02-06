ecd1b1876e27084809d8c6f285341d03fdc49aa8
Minjeong-Kwak <cynicalrei@gmail.com>
2025-02-06 20:51:19 +0900

    Add files via upload

---
+++ memory.py
@@ -0,0 +1,19 @@
+import os
+import stramlit as st
+import tempfile
+
+from langchain.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.putput_parsers import StrOutputParser
+
+chat_history = StreamlitChatMessageHistory()
+
+__import__('pysqlite3')
+import sys
+sys.modules['splite3'] = sys.modules.pop('pysqlite3')
+
+from langchain.vectorstores import Chroma
+os.environ["OPENAI_API_KEY"] = "your api key"
